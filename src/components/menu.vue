63fbb2b081089f8b49a045f28aca3bac6689a8ab
ali atarmoghadam <techzero@sabinarya.com>
2018-10-31 17:37:54 +0330

    init commit

---
+++ src/components/menu.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="fixed-top navbar navbar-expand-lg navbar-light bg-light bg-white ">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="../assets/favicon.png">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                      <router-link to="/" :event="disabled ? '' : 'click'" class="nav-link" disabled>خانه</router-link>
+                    <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
+                </li>
+                <li class="nav-item">
+                      <router-link to="/" :event="disabled ? '' : 'click'" class="nav-link" disabled>صدور قبض</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/" :event="disabled ? '' : 'click'" class="nav-link" disabled>پرداخت قبض</router-link>
+                </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'Menu',
+}
+</script>
+<style>
+.logo {
+    width: 50px;
+}
+.bg-white {
+    background-color: #f7f7f7;
+    border-bottom: 1px solid #cacaca;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="سامانه صدور و پرداخت قبوض"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
+
+<style>
+@font-face {              
+     font-family: sabin;
+     src: url('../assets/fonts/Yekan.ttf');
+}
+
+h1, h2, h3, p, span {
+  font-family: sabin
+}
+
+.home {
+  position: absolute;
+  top: 35%;
+  left: 35%;
+}
+
+</style>
